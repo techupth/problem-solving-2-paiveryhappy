a201b1e1ca96d4142d1b0e97a1b6b36759abc0f5
Jiran <paiveryhappy@gmail.com>
2024-02-13 01:50:47 +0700

    solved problem-solving

--- ex-1.js
+++ ex-1.js
@@ -1,9 +1,34 @@
 //Exercise 1: Roman to Integer
 
 const romanToInt = function (s) {
-  //Start coding here
+  const Alphabet = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    // เช็คว่าถ้าตัวถัดไปมีค่ามากกว่าตัวปัจจุบันหรือไม่
+    if (i < s.length - 1 && Alphabet[s[i]] < Alphabet[s[i + 1]]) {
+     
+      // ถ้าใช่ ให้นำค่าของตัวปัจจุบันไปลบออกจากผลลัพธ์
+
+      result = result-Alphabet[s[i]];
+    } else {
+      // ถ้าไม่ใช่ ให้นำค่าของตัวปัจจุบันเพิ่มเข้าไปในผลลัพธ์
+      result += Alphabet[s[i]];
+    }
+  }
+
+  return result;
 };
 
 const result1 = romanToInt("III"); // 3
 const result2 = romanToInt("LVIII"); // 58
 const result3 = romanToInt("MCMXCIV"); //1994
+
+console.log(result1, result2, result3);
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,19 @@
 //Exercise 2: Fizz Buzz
 
-const fizzBuzz = function (n) {
-  //Start coding here
+const fizzBuzz = function (number) {
+  const result = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(String(i));
+    }
+  }
+  return result;
 };
 
 const result1 = fizzBuzz(3);
